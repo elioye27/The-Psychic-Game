3016d93a28f424ed039be71adde557097aab1732
elioye27 <elioye27@gmail.com>
2017-09-02 16:47:44 -0700

    add final project

---
+++ assets/javascript/game.js
@@ -0,0 +1,53 @@
+// Creates an array that lists out all of the words.
+var letters = [
+    "a", "h", "l"
+]
+
+
+// Creating variables to hold the number of wins, guessed remaining, and letters Already used. They start at 0 or set guesses left at 9.
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+
+
+
+
+// This function is run whenever the user presses a key.
+document.onkeyup = function(event) {
+
+    // Determines which key was pressed.
+    var userGuess = event.key;
+
+    // Randomly chooses a choice from the options array.
+    var letterGuessed = letters[Math.floor(Math.random() * letters.length)];
+
+
+    // This logic determines the outcome of the game (win/loss/guessesleft), and increments/decrements the appropriate number
+
+    for (var i = 0; i < letters.length; i++) {
+
+        if ((userGuess == letters[i]) && (letterGuessed === letters[i])) {
+            wins++;
+        } else if ((userGuess !== letters[i]) && (letterGuessed === letters[i])) {
+            losses++;
+            guessesLeft--;
+        } else if (guessesLeft === 0) {
+            break;
+        }
+
+
+
+        // Creating a variable to hold our new HTML. Our HTML now keeps track of the user and computer guesses, and wins/losses/guesses left.
+        var html =
+            "<h1>Guess what letter I'm thinking of" + "</h1>" + "<br>" +
+            "<p>Wins: " + wins + "</p>" + "<br>" +
+            "<p>Losses: " + losses + "</p>" + "<br>" +
+            "<p>Guesses Left: " + guessesLeft + "</p>" + "<br>" +
+            "<p>Guesses So Far: " + userGuess + ", " + userGuess + ", " + "</p>"
+
+
+        // Set the inner HTML contents of the #psychic div to our html string
+
+        document.querySelector("#psychic").innerHTML = html;
+    }
+};
